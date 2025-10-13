24f3f69746bd89120e342e1523c60ada0409d927
Lukas G. Aarhus <lukas.grahl@econ.au.dk>
2025-10-13 09:21:36 +0200

    added args parse

--- lukas_utils/__init__.py
+++ lukas_utils/__init__.py
@@ -7,7 +7,7 @@ from .helpers_data import (
 from .helpers_plot import get_ndf_plot, get_2d_df_figure, get_fig_axes
 from .helpers_sql import get_sql_connection, get_sql_tab_from_df, write_df_to_sql
 from .helpers_logging import get_logger
-from .utils import chunk_it, run_paralle_dec
+from .utils import chunk_it, run_paralle_dec, ArgParseArgument, obj_parse_n_process
 
 __all__ = [
     "df_cast_data",
@@ -23,4 +23,6 @@ __all__ = [
     "lst_df_merge_dt_freq",
     "get_freq_sparse_df",
     "df_merge_dt_freq",
+    "ArgParseArgument",
+    "obj_parse_n_process",
 ]
