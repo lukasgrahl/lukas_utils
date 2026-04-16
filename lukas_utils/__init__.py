18e76b39a3e205273ec7ade00204d7b0b40eb96a
Lukas G. Aarhus <lukas.grahl@econ.au.dk>
2026-04-16 13:32:45 +0200

    added winsorise

--- lukas_utils/__init__.py
+++ lukas_utils/__init__.py
@@ -8,6 +8,7 @@ from .helpers_plot import get_ndf_plot, get_2d_df_figure, get_fig_axes
 from .helpers_sql import get_sql_connection, get_sql_tab_from_df, write_df_to_sql
 from .helpers_logging import get_logger
 from .utils import chunk_it, run_paralle_dec, ArgParseArgument, obj_parse_n_process
+from .helpers_stats import winsorise, adf_test_summary
 
 __all__ = [
     "df_cast_data",
@@ -25,4 +26,6 @@ __all__ = [
     "df_merge_dt_freq",
     "ArgParseArgument",
     "obj_parse_n_process",
+    "winsorise",
+    "adf_test_summary",
 ]
